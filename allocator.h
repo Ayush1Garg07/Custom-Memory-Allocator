5cc0c51750f9e8ea9efd418cb63783b38e8c56f7
Ayush Garg <ayush.garg1528@gmail.com>
2026-01-04 01:20:41 +0530

    Added heap function

--- allocator.h
+++ allocator.h
@@ -12,6 +12,7 @@ typedef struct BlockHeader {
     size_t size;
     int free;
     struct BlockHeader* next;
+    struct BlockHeader* next_free;
 } BlockHeader;
 
 typedef enum{
@@ -32,5 +33,6 @@ void* my_malloc(size_t size);
 void  my_free(void* ptr);
 void* my_calloc(size_t n, size_t size);
 void* my_realloc(void* ptr, size_t size);
+void print_heap();
 
 #endif
